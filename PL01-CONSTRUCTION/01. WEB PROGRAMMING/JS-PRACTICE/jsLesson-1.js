7b9ee6f4f2ca2b205f901a75e2f5cd4adc5cfb86
SM-SHAMEEM <rony.shameem@gmail.com>
2022-12-01 20:50:19 +0600

    PL01 Lesson-01 - variable | let | const | scope | Hosting | strict mode

--- PL01-CONSTRUCTION/01. WEB PROGRAMMING/JS-PRACTICE/jsLesson-1.js
+++ PL01-CONSTRUCTION/01. WEB PROGRAMMING/JS-PRACTICE/jsLesson-1.js
@@ -1,5 +1,31 @@
-const out = document.createElement('div');
-document.body.appendChild(out);
-out.setAttribute('id', 'styleOne');
+// const out = document.createElement('div');
+// document.body.appendChild(out);
+// out.setAttribute('id', 'styleOne');
 
-out.innerHTML = 'JavaScript';
+// out.innerHTML = 'JavaScript';
+
+const condition = true;
+const promiseOne = new Promise((resolve, reject) => {
+    if (condition == true)
+        resolve('request resolved ...');
+    else 
+        reject('request rejected ....');
+});
+
+promiseOne
+    .then((resolved) => {
+        console.log(resolved);
+    })
+    .catch((rejected) => {
+        console.log(rejected);
+    });
+
+console.log('promise - then');
+
+async function promiseFunc() {
+    const result = await promiseOne;
+    console.log(result);
+}
+
+promiseFunc();
+console.log('promise - async');
